694791e0f392cf65e340003c681f4fd16ec0d041
angalland <angalland@laposte.net>
2023-05-23 11:23:24 +0200

    ajout de commentaire

--- PDO.php
+++ PDO.php
@@ -8,15 +8,15 @@
 </head>
 <body>
 <?php
-try {
-$db = new PDO(
-    'mysql:host=localhost;dbname=gaulois',
-    'root',
-    ''
+try { // on effectue un "test" pour voir si on a bien connecté php a Mysql 
+$db = new PDO(   // on se connecte a MySQL
+    'mysql:host=localhost;dbname=gaulois', // on donne le nom d'hôte ici on est en local donc localhost, le nom de la base de donné : gaulois
+    'root', // on indique ici le nom d'utilisateur
+    ''  // on indique ici le mot de passe : ici il n y a pas de mot de passe donc ''
 );
 
 }
-catch (Execption $e)
+catch (Execption $e)  // si la connextion n'a pas eu lieu affichera ce message d'erreure
 {
     die('Erreur : ' . $e->getMessage());
 }
@@ -33,10 +33,10 @@ $personnagesStatement = $db->prepare($sqlQuery);
 $personnagesStatement->execute();
 $personnages = $personnagesStatement->fetchAll();
 
-$sqlQuery1 = 'SELECT * FROM specialite';
-$specialiteStatement = $db->prepare($sqlQuery1);
-$specialiteStatement->execute();
-$specialites = $specialiteStatement->fetchAll();
+$sqlQuery1 = 'SELECT * FROM potion';
+$potionStatement = $db->prepare($sqlQuery1);
+$potionStatement->execute();
+$potions = $potionStatement->fetchAll();
 
 ?>
 <table>
